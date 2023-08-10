7339f0f282b44c5e817665daba69bb3ead06d942
PJiyush <jhamnani.1@iitj.ac.in>
2023-08-10 15:39:10 +0530

    created the structure of navbar

---
+++ community-page/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <div className='completeNav'>
+        <div className='logo' >
+            <img src="" alt="" />
+            Plutonn
+        </div>
+        <div className='pageHeadline'> Community - Posts </div> 
+        <div className='rightSide' >Profile</div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
